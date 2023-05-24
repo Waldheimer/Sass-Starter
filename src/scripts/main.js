27825257b4b66bc8bda8c013bc9b47a914d8a817
Markus Clemens <whm20060@outlook.de>
2023-05-24 11:21:11 +0000

    Updated main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,3 @@
-import { loadEvents, loadEntities, loadAutomations, loadServices } from "./data.js"
-
-/// -----
-//  Load all Data from the Timetable
-/// -----
-const events = loadEvents()
-const entities = loadEntities()
-const automations = loadAutomations()
-const services = loadServices()
-
-
-
-
-
-
-
 
 ///
 //  Start everything
